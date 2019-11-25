2fb493e7f2aa285126a4a4b4ea31cc981a70bccc
Elouan MARKGRAF <elouan.markgraf@sio.lan>
2019-11-25 08:36:21 +0100

    ajout pageTech

---
+++ pageTechnicien.php
@@ -0,0 +1,34 @@
+<<!DOCTYPE html>
+<?php
+
+
+
+?>
+
+
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CashCash</title>
+  </head>
+
+  <body>
+      <header>
+          <h1>CashCash</h1>
+      </header>
+
+      <nav>
+
+      </nav>
+
+      <input type="text" name="numero" value="" placeholder="N°">
+      <input type="text" name="numero" value="" placeholder="Date affection">
+      <input type="text" name="numero" value="" placeholder="Date fin">
+      <input type="text" name="numero" value="" placeholder="Priorité">
+
+      <input type="button" name="" value="Recherche">
+
+  </body>
+</html>
